82baeb904395d3bf10ad2094c9c7b2006f29998e
fejustino <fwxjustino@gmail.com>
2023-09-09 15:52:51 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import fastify from 'fastify'
+import { knex } from './database'
+const server = fastify()
+
+server.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
+
+server
+  .listen({ port: 3333 })
+  .then(() => { console.log('Running...') })
+  .catch(() => { console.log('Error...') })
