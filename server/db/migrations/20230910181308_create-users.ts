11df8d78b7efb17aa1a46b54c327298da73189cc
fejustino <fwxjustino@gmail.com>
2023-09-10 16:54:16 -0300

    refact: updating database

---
+++ server/db/migrations/20230910181308_create-users.ts
@@ -0,0 +1,27 @@
+import { type Knex } from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('ID').notNullable().primary()
+    table.uuid('idUsuario').notNullable().index()
+    table.timestamp('DataHoraCadastro').defaultTo(knex.fn.now()).notNullable()
+    table.string('Codigo', 15).notNullable()
+    table.string('Nome', 150).notNullable()
+    table.string('CPF_CNPJ', 20).notNullable()
+    table.integer('CEP').notNullable()
+    table.string('Logradouro', 100).notNullable()
+    table.string('Endereco', 120).notNullable()
+    table.string('Numero', 20).notNullable()
+    table.string('Bairro', 50).notNullable()
+    table.string('Cidade', 60).notNullable()
+    table.string('UF', 2).notNullable()
+    table.string('Complemento', 150).notNullable()
+    table.string('Fone', 15).notNullable()
+    table.float('LimiteCredito').notNullable()
+    table.date('Validade').notNullable()
+  })
+}
+
+export async function down (knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
